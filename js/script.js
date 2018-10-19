0bd517b7a4e96a85cb51df0ce84b03c3ea5f26d0
Matthew Wong <matthewaramis@MacBook-Air-3.local>
2018-10-19 16:08:50 -0700

    Began writing API script. Added some variables and mixins to .scss partials.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,26 @@
 $(document).ready(function() {
   // js code
-  console.log("hello world");
+  $("select").on("change", function(){
+    
+    const topic-select = $('.topicselector').val(); 
+    
+    const url= `https://api.nytimes.com/svc/topstories/v2/${"topic-select"}.json`;
+
+    url += '?' + $.param({
+      'api-key': "17652fbd85734f4f98836d7c455a7164"
+    });
+    $.ajax({
+
+      method: "GET",
+    })done(function(data) {
+      $(".stories").empty();
+
+      $.each(data, function(key, value) {
+        
+      })
+    })
+
+  })
 });
+
+
